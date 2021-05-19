cbfa4f0c0f37b0750a456f40b693d2dc0959243d
Alejandro Garrido Mota <garridomota@gmail.com>
2021-05-19 15:29:47 +0100

    Added tf-docs and remove dependences variables

--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "dependence_opa" {
-  description = "OPA module dependences in order to be executed."
-}
-
-variable "dependence_prometheus" {
-  description = "Prometheus module dependences in order to be executed."
-}
